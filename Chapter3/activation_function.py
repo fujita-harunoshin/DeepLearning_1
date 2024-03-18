49d4680f29104ea8f7ac6cb3120ef4e235fa13ea
Harunoshin Fujita <fujita.harunoshin@gmail.com>
2024-03-18 21:18:56 +0900

    活性化関数の作成

--- Chapter3/activation_function.py
+++ Chapter3/activation_function.py
@@ -1,6 +1,10 @@
 import numpy as np
-import matplotlib.pylab as plt
 
 def step_function(x):
-  y = x > 0
-  return y.astype()
+  return np.array(x > 0, dtype=int)
+
+def sigmoid(x):
+  return 1 / (1 + np.exp(-x))
+
+def relu(x):
+  return np.maximum(0, x)
